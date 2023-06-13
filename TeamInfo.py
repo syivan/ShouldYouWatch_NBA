3834a2ef0004a969d77d23e0f6c92bcf1bb37031
syivan <ivansyrups@gmail.com>
2023-06-13 02:53:07 -0700

    team information constructed

---
+++ TeamInfo.py
@@ -0,0 +1,12 @@
+TEAMNAMES = ['hawks', 'celtics', 'nets', 'hornets', 'bulls', 'cavaliers',
+             'mavericks', 'nuggets', 'pistons', 'warriors', 'rockets',
+             'pacers', 'clippers', 'lakers', 'grizzlies', 'heat', 'bucks',
+             'timberwolves', 'pelicans', 'knicks', 'thunder', 'magic',
+             'sixers', 'suns', 'trailblazers', 'kings', 'spurs', 'raptors',
+             'jazz', 'wizards']
+TEAM_ABBREVIATIONS = ['atl', 'bos', 'bkn', 'cha', 'chi', 'cle',
+                      'dal', 'den', 'det', 'gsw', 'hou',
+                      'ind', 'lac', 'lal', 'mem', 'mia', 'mil',
+                      'min', 'nop', 'nyk', 'okc', 'orl',
+                      'phi', 'phx', 'por', 'sac', 'sas', 'tor'
+                      'uta', 'was']
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import requests
 from nba_api.live.nba import endpoints
 from matplotlib import pyplot as plt
 
+def verifyUserInput():
+    teamNames=['Hawks', 'Celtics', 'Nets', 'Hornets',]
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -13,6 +15,14 @@ if __name__ == '__main__':
     dataToday = endpoints.scoreboard.ScoreBoard().get_dict()
     # Our "data" variable now has built in functions such as creating a dataframe for our data
     gameData = dataToday.get('scoreboard').get('games')
-    print(gameData)
+    teamToWatch = input("What Team to Watch\n")
     print(dataToday)
+    for i in range(len(gameData)):
+        if (gameData[i].get('homeTeam').get('teamName') == teamToWatch):
+            print('[%s]: %d' % (teamToWatch, gameData[i].get('homeTeam').get('score')))
+        print(gameData[i])
+
+    #r = requests.get("http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2020/league/00_full_schedule.json")
+    #print(type(r))
+    #print(r.json())
 
