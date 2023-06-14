3ab6567b6c0f3db1f4848cb36b9391784eb84c09
syivan <ivansyrups@gmail.com>
2023-06-14 00:57:55 -0700

    specific Team Script - Boston Celtics

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 # First we will import our packages
-import requests
 from nba_api.live.nba import endpoints
-import TeamInfo
-def verifyUserInput():
-    while True:
-        teamToWatch = input("What Team to Watch\n")
-        if (teamToWatch in TeamInfo.TEAM_NAMES or TeamInfo.TEAM_ABBREVIATIONS):
-            print("Team Present in User Input")
-    return teamToWatch
 
-# Press the green button in the gutter to run the script.
+# MAIN
 if __name__ == '__main__':
-    # Here we access the leagueleaders module through endpoints & assign the class to "data"
+    # Access scoreboard modules in dictionary format
     dataToday = endpoints.scoreboard.ScoreBoard().get_dict()
-    # Our "data" variable now has built in functions such as creating a dataframe for our data
+    # gameData contains a value of games being played today
     gameData = dataToday.get('scoreboard').get('games')
-    teamToWatch = verifyUserInput()
+
+    # Future: Plan to implement input to access different teams
+    # teamToWatch = verifyUserInput()
+
+    myTeam = 'Celtics'
+    print(gameData)
     print(dataToday)
     for i in range(len(gameData)):
-        if (gameData[i].get('homeTeam').get('teamName') == teamToWatch):
-            print('[%s]: %d' % (teamToWatch, gameData[i].get('homeTeam').get('score')))
-        print(gameData[i])
-
-    #r = requests.get("http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2020/league/00_full_schedule.json")
-    #print(type(r))
-    #print(r.json())
-
+        # Checks in the iterated games array if the Celtics play in the games today
+        currentHomeTeam = gameData[i].get('homeTeam')
+        currentAwayTeam = gameData[i].get('awayTeam')
+        if currentHomeTeam.get('name') == myTeam or currentAwayTeam.get('name') == myTeam:
+            print('[%s]: %d\n[%s]: %d'.format(currentHomeTeam.get('name'),
+                                              currentHomeTeam.get('score'),
+                                              currentAwayTeam.get('name'),
+                                              currentAwayTeam.get('score')))
+            
